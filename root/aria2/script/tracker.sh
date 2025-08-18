0bc93b6ba7d8774e5b81987ba47dd76723399611
SKIYET <49865295+SKIYET@users.noreply.github.com>
2025-08-19 00:26:46 +0800

    Update tracker.sh

--- root/aria2/script/tracker.sh
+++ root/aria2/script/tracker.sh
@@ -28,8 +28,8 @@ GET_TRACKERS() {
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers..."
         TRACKER=$(
             ${DOWNLOADER} https://trackerslist.com/all_aria2.txt ||
-                ${DOWNLOADER} https://cdn.jsdelivr.net/gh/XIU2/TrackersListCollection@master/all_aria2.txt ||
-                ${DOWNLOADER} https://trackers.p3terx.com/all_aria2.txt
+            ${DOWNLOADER} https://raw.githubusercontent.com/Tunglies/TrackersList/refs/heads/main/all.txt ||
+                ${DOWNLOADER} https://ghproxy.com/https://raw.githubusercontent.com/Tunglies/TrackersList/main/all.txt 
         )
     else
         echo && echo -e "$(DATE_TIME) ${INFO} Get BT trackers from url(s):${CTU} ..."
